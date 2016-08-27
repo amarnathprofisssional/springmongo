a1f720f7aa88580690292ac88f518438ba40d577
Amar T <amarnathprofissional@gmail.com>
2016-08-27 08:54:18 +0530

    Working Changes

--- src/main/java/com/test/webapp/repo/CustomerRepository.java
+++ src/main/java/com/test/webapp/repo/CustomerRepository.java
@@ -3,10 +3,13 @@ package com.test.webapp.repo;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import com.test.webapp.dao.Customer;
 
-
+@Repository
+@Component
 public interface CustomerRepository extends MongoRepository<Customer,String> {
 
 	public Customer findByFirstName(String firstName) ;
